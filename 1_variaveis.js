bcded7e72b8e6a74a64bc9ab1df773339446dd42
carloeira <augustovieira.cb@gmail.com>
2024-09-30 09:02:47 -0300

    variaveis

---
+++ 1_variaveis.js
@@ -0,0 +1,43 @@
+// declaração de variáveis
+
+// let idade = 19;
+// var isGamer = false;
+// const nome = 'Carlos';
+// console.log(idade);
+
+// redeclaração
+
+// let idade = 30; // Erro: Não permite redeclaração
+// var isGamer = true;
+// const nome = 'João'; // Erro: Não permite redeclaração
+
+// reatribuição
+
+// console.log(idade);
+// idade = 29;
+// isGamer = false;
+// nome = 'Marcos';
+
+// Hoisting (Içamento / elevação) somente var
+
+// salgado = 9;
+// console.log(salgado, pratinho);
+// var pratinho = 15;
+
+// var salgado = 9;
+// console.log(salgado, pratinho);
+// var pratinho = 15;
+
+// Possíveis maneiras de declarar variáveis
+
+let name123;
+let Name123;
+let $name123;
+let _name123;
+
+// Não dá pra fazer
+
+// let 123Name123;
+// let @123name;
+// let "name";
+// let 'name';
