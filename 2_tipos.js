340b3930067607252a32cdb2ff2fdf98c0011935
carloeira <augustovieira.cb@gmail.com>
2024-09-30 12:03:46 -0300

    tipos

---
+++ 2_tipos.js
@@ -0,0 +1,44 @@
+// Primitivos
+
+// typeof 10;
+// typeof 'Carlos';
+// typeof true;
+// typeof null;
+// typeof undefined;
+
+// Métodos para number
+
+// (10).toFixed(2);
+// parseInt(10,50);
+// parseFloat(10,6);
+
+// Métodos para strings
+
+// ('JavaScript').toUpperCase();
+// ('JavaScript').replace('a', 4);
+// const result = 'JavaScript'.replace(/a/g, 4);
+// (true).toString();
+
+// let idade = 19;
+// let nome = 'Carlos';
+// let isGamer = false;
+
+// console.log("Nome: " + nome, "Idade: " + idade, "Joga online? " + isGamer);
+// console.log(`
+//         Nome: ${nome},
+//         Idade: ${idade},
+//         Joga Online? ${isGamer}
+//     `);
+
+// Objetos
+
+// const carro = { marca: 'Fiat' };
+// console.log(carro);
+
+// let numbers = [1, 2, 3, 4];
+
+
+// instanceof
+
+// console.log(42, instanceof Number);
+// console.log(new String('TESTE') instanceof Number);
